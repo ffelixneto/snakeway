c6b7c68075e7901b74ae94877d60c5cd3ae81006
ffelixneto <ffelixneto@outlook.com>
2020-02-05 20:19:20 -0300

    Correção de arquivos.

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 ### Projeto de RPG em interface de linha de comando.
 
 ##### Colaboradores:
+- [Felix Neto](https://github.com/ffelixneto)
 - [Moisés Abraão](https://github.com/MoisesAbraao)
--- utils.py
+++ utils.py
@@ -1,19 +1,22 @@
 # -*- coding: utf-8 -*-
 import os
 import time
+
 # Função para limpar a tela
 def clear():
     os.system('cls' if os.name =='nt' else 'clear')    
 
-
-def texto(texto, tempo):
-    t = texto.decode('utf-8')
+# Função básica para exibir textos
+def texto(texto, tempo=2):
     print('_____________________________________________________________________________________')
-    print(t)
+    print(texto)
     print('_____________________________________________________________________________________')
     time.sleep(tempo)
 
-def texto_cidade(texto):
+# Função básica para exibir textos de seleção de cidades
+def texto_cidade(texto, tempo=2):
     t = texto.decode('utf-8')
     print(t)
     print('_____________________________________________________________________________________')
+    time.sleep(tempo)
+    clear()
